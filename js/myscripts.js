91970484ce351e671e94997c2f0e5c06fe19de23
iampamelas <pamela@pamela-s.com>
2020-06-11 11:24:37 +0200

    adding mobile homepage

---
+++ js/myscripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myNav").style.left = "0";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.left = "-100vw";
+  }
+  
