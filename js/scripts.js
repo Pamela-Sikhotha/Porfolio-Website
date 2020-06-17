f351edad4c46de3a33266ad40d6902d3fa8a56db
iampamelas <pamela@pamela-s.com>
2020-06-17 16:19:25 +0200

    homepage

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function openNav() {
+    document.getElementById("myNav").style.left = "0";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.left = "-100vw";
+  }
+  
+
+
+
+
+// function openNav() {
+//     document.getElementById("myNav").style.display = "block";
+//   }
+  
+//   function closeNav() {
+//     document.getElementById("myNav").style.display = "none";
+//   }
+  
